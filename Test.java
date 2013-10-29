5671a03369370bdf06ddd57c687957467682b26c
Ian Zapolsky <ianzapolsky@gmail.com>
2013-10-29 13:05:08 -0300

    added all initial files

---
+++ Test.java
@@ -0,0 +1,8 @@
+public class Test {
+
+    public static void main(String[] args) {
+        SudokuBoard puzzle = new SudokuBoard();
+        SudokuSolver genius = new SudokuSolver();
+        genius.solve(puzzle);
+    }
+}
